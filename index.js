62cdfc555aadd7de6329753c460820ced877e0db
Alarick Whitted <awhitted834@gmail.com>
2023-12-31 12:24:22 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  function fuzzyMatch(drivers, partialName) {
+    return drivers.filter(driver => driver.startsWith(partialName));
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
